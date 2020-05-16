c99d87d46fcc450a8015cb7dadee8d83f8dd74c7
chihirotanaka <senno330@gmail.com>
2020-05-16 18:33:15 +0000

    [add]quizビューページ

--- app/controllers/playquizzes_controller.rb
+++ app/controllers/playquizzes_controller.rb
@@ -3,11 +3,11 @@ class PlayquizzesController < ApplicationController
  before_action :set_elements, only: %i(play_quiz result_quiz total_quiz) 
 
   def play_quiz
- 
- 
+  	@quiz = Quiz.find(params[:id])
   end
 
   def result_quiz
+  	@answer = Quiz.find(params[:answer][:movie_id])
   	
   end
 
